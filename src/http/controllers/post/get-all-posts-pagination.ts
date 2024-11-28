719ff144c2264258a4b86ce78b504f60e57ad7b5
Diego Gervásio <diegox300@gmail.com>
2024-11-28 16:24:20 -0300

    add: Pagination in all posts.

---
+++ src/http/controllers/post/get-all-posts-pagination.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express'
+import { makeGetAllPostsPaginationUseCase } from '../../../use-cases/factory/make-get-all-posts-pagination-usecase'
+
+// Get all posts with pagination
+export const getAllPostsPagination = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const getAllPostsPaginationUseCase = makeGetAllPostsPaginationUseCase()
+
+  const page = parseInt(req.query.page as string) || 1 // Get the page number from query parameters, default to 1 if not provided
+
+  getAllPostsPaginationUseCase
+    .execute(page)
+    .then((result) => res.status(200).send(result))
+    .catch(next) // Pass the error to the error handling middleware
+}
