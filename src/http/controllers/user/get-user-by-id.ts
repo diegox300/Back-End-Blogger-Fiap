bbdbf2ab5811dcb12629125ba696523ac641e41b
Diego Gervásio <diegox300@gmail.com>
2024-12-10 19:02:25 -0300

    fix: error user id

--- src/http/controllers/user/get-user-by-id.ts
+++ src/http/controllers/user/get-user-by-id.ts
@@ -2,17 +2,18 @@ import { Request, Response, NextFunction } from 'express'
 import { makeFindUserByIdUseCase } from '../../../use-cases/factory/user/make-find-user-by-id-usecase' // Importing the factory function to create the use case for finding users by ID
 import { asyncHandler } from '../../../middleware/asyncHandler' // Importing middleware for handling async operations
 import { validateObjectId } from '../../../middleware/validateObjectId'
-import { UserNotFoundError } from '../../../errors/UserNotFoundError'
 
 // Get a user by ID
-export const getUserById = [validateObjectId]
-asyncHandler(async (req: Request, res: Response) => {
-  const { id } = req.params // Get the ID from the request parameters
-  const findUserByIdUseCase = makeFindUserByIdUseCase() // Creating an instance of the use case for finding users by ID
+export const getUserById = [
+  validateObjectId,
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params // Get the ID from the request parameters
+    const findUserByIdUseCase = makeFindUserByIdUseCase() // Creating an instance of the use case for finding users by ID
 
-  // Handle the retrieval of the user and wait for the result
-  const user = await findUserByIdUseCase.handler(id)
+    // Handle the retrieval of the user and wait for the result
+    const user = await findUserByIdUseCase.handler(id)
 
-  // Send a 200 OK response with the user data
-  res.status(200).send(user)
-})
+    // Send a 200 OK response with the user data
+    res.status(200).send(user)
+  }),
+]
--- src/use-cases/user/find-user-by-id-usecase.ts
+++ src/use-cases/user/find-user-by-id-usecase.ts
@@ -5,11 +5,14 @@ export class FindUserByIdUseCase {
   constructor(private userRepository: UserRepository) {} // Injecting the UserRepository dependency
 
   async handler(id: string) {
+    console.log(`Finding user with ID: ${id}`) // Log the ID being searched
     // Method to execute the retrieval of a user by ID
     const user = await this.userRepository.findById(id)
     if (!user) {
+      console.log(`User with ID: ${id} not found`) // Log if the user is not found
       throw new UserNotFoundError()
     }
+    console.log(`User with ID: ${id} found: ${JSON.stringify(user)}`) // Log the found user
     return user
   }
 }
